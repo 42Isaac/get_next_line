ae636de1d34acd1bf7492dd640436ba9e37680d0
Isaac Perez <23imperez@gmail.com>
2019-10-03 13:09:53 -0700

    better gnl tester

--- gnl includes/main.c
+++ gnl includes/main.c
@@ -49,13 +49,73 @@ int				main(int ac, char **av)
 	char	*line;
 	int		i1;
 	int		i2;
+	int		i3;
 	int		fd1;
 	int		fd2;
+	int		fd3;
 
-	if (ac < 2 && ac > 3)
+	if (ac < 2 || ac > 4)
 	{
-		write(1, &"\n", 1);
+		ft_putstr("usage:\n	Enter 1 - 3 paramaters for gnl to process.\n");
+		ft_putstr("	Output will print file descriptor number,\n	followed by what gnl returned,\n	followed by the string 'line' that gnl wrote to.\n");
 		return (0);
+	}
+	if (ac == 4)
+	{
+		i1 = 1;
+		i2 = 1;
+		i3 = 1;
+		fd1 = open(av[1], O_RDONLY | O_EXCL);
+		fd2 = open(av[2], O_RDONLY | O_EXCL);
+		fd3 = open(av[3], O_RDONLY | O_EXCL);
+		while (i1 == 1 || i2 == 1 || i3 == 1)
+		{
+			if (i1 == 1)
+			{
+				i1 = get_next_line(fd1, &line);
+				ft_putstr("FD: ");
+				ft_putnbr(fd1);
+				ft_putstr(" Output: ");
+				ft_putnbr(i1);
+				if (i1 == 1)
+				{
+					ft_putstr(" line: ");
+					ft_putstr(line);
+					free(line);
+				}
+				ft_putstr("\n");
+			}
+			if (i2 == 1)
+			{
+				i2 = get_next_line(fd2, &line);
+				ft_putstr("FD: ");
+				ft_putnbr(fd2);
+				ft_putstr(" Output: ");
+				ft_putnbr(i2);
+				if (i2 == 1)
+				{
+					ft_putstr(" line: ");
+					ft_putstr(line);
+					free(line);
+				}
+				ft_putstr("\n");
+			}
+			if (i3 == 1)
+			{
+				i3 = get_next_line(fd3, &line);
+				ft_putstr("FD: ");
+				ft_putnbr(fd3);
+				ft_putstr(" Output: ");
+				ft_putnbr(i3);
+				if (i3 == 1)
+				{
+					ft_putstr(" line: ");
+					ft_putstr(line);
+					free(line);
+				}
+				ft_putstr("\n");
+			}
+		}
 	}
 	if (ac == 3)
 	{
@@ -63,21 +123,37 @@ int				main(int ac, char **av)
 		i2 = 1;
 		fd1 = open(av[1], O_RDONLY | O_EXCL);
 		fd2 = open(av[2], O_RDONLY | O_EXCL);
-		while (i1 || i2)
+		while (i1 == 1 || i2 == 1)
 		{
-			if (i1)
+			if (i1 == 1)
 			{
 				i1 = get_next_line(fd1, &line);
-				ft_putstr(line);
+				ft_putstr("FD: ");
+				ft_putnbr(fd1);
+				ft_putstr(" Output: ");
+				ft_putnbr(i1);
+				if (i1 == 1)
+				{
+					ft_putstr(" line: ");
+					ft_putstr(line);
+					free(line);
+				}
 				ft_putstr("\n");
-				free(line);
 			}
-			if (i2)
+			if (i2 == 1)
 			{
 				i2 = get_next_line(fd2, &line);
-				ft_putstr(line);
+				ft_putstr("FD: ");
+				ft_putnbr(fd2);
+				ft_putstr(" Output: ");
+				ft_putnbr(i2);
+				if (i2 == 1)
+				{
+					ft_putstr(" line: ");
+					ft_putstr(line);
+					free(line);
+				}
 				ft_putstr("\n");
-				free(line);
 			}
 		}
 	}
@@ -85,21 +161,22 @@ int				main(int ac, char **av)
 	{
 		i1 = 1;
 		fd1 = open(av[1], O_RDONLY | O_EXCL);
-		while (i1)
+		while (i1 == 1)
 		{
 			i1 = get_next_line(fd1, &line);
-			if (i1 == 0)
-			{
-				ft_putstr("ZERO\n");
-				break;
-			}
-			ft_putstr(line);
-			free(line);
-			ft_putstr(": ");
+			ft_putstr("FD: ");
+			ft_putnbr(fd1);
+			ft_putstr(" Output: ");
 			ft_putnbr(i1);
+			if (i1 == 1)
+			{
+				ft_putstr(" line: ");
+				ft_putstr(line);
+				free(line);
+			}
 			ft_putstr("\n");
 		}
 	}
-	ft_putstr("\n~-~ EOF HAS BEEN REACHED ~-~\n");
+	ft_putstr("\n\n~-~ EOF HAS BEEN REACHED ~-~\n");
 	return (0);
 }
